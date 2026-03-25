00bc8c074ab0ebdc96679b475026839ba2ede8dd
Ron B <rberle@usf.edu>
2026-03-25 11:19:03 -0400

    adding Staff model

--- Competition2/Models/Staff.cs
+++ Competition2/Models/Staff.cs
@@ -1,4 +1,9 @@
-public class Staff
+namespace Competition2.Models
 {
+    public class Staff : People
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+    }
     
 }
