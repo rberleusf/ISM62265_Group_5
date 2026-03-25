54b480c7de7b57f38cc62efe17f8bdd15af816de
Amer Amer <aamer@usf.edu>
2026-03-25 11:28:42 -0400

    Checking in Student

---
+++ Competition2/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Competition2.Models
+{
+    public class Student : People
+    { 
+        public string MajorName { get; set; }
+        public string GraduationYear { get; set; }
+    }
+}
