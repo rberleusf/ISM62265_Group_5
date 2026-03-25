1267dac1f4b70b1f4566d2c179220eb4e19a7e4c
Victorita Romanov <vromanov@usf.edu>
2026-03-25 11:13:44 -0400

    Checking in people

---
+++ Competition2/Models/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Competition2.Models
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+    }
+    
+}
